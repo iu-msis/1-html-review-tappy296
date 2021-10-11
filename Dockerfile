b6ca1ffd3524bf955b4997d9315f6ce44cee3760
tappy296 <coletappy@gmail.com>
2021-10-11 12:41:06 -0400

    book API building

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM php:7.4-apache
 
-LABEL maintainer="Tom Gregory"
+LABEL maintainer="Cole Tapiaqaguilar"
 
 RUN docker-php-ext-install pdo_mysql
 
---
+++ app/public/js/books.js
@@ -0,0 +1,32 @@
+   
+const book = {
+    data() {
+      return {
+        "book": [],
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        fetchBookData() {
+            fetch('api/book')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.book = parsedJson;
+                console.log("C");
+            })
+            .catch( err => {
+                console.error(err)
+            })
+
+            console.log("B");
+        }
+    },
+    created() {
+        this.fetchBookData();
+    }
+  }
+  
+Vue.createApp(Offer).mount('#bookApp');
